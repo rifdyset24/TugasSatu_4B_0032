a9d5ecc706754793f5c3a820f555cd44d7031bfa
rifdyset45 <rifdysetyadi906@gmail.com>
2026-05-08 15:52:41 +0700

    implementation task input validation

--- script.js
+++ script.js
@@ -4,5 +4,11 @@ const inputTanggal = document.getElementById("tanggal");
 const daftarTugas = document.getElementById("listTugas");
 
 btnTambahTodo.addEventListener("click",function(){
+    
+    if(inputValue.value === ""){
+        alert("input tidak boleh kosong!");
+        return;
+    }
+
 
 });
