4b8f9c9f447c22058e8efaacaba379b8f5f899d5
rifdyset45 <rifdysetyadi906@gmail.com>
2026-05-08 15:54:25 +0700

    create dynamic task item

--- script.js
+++ script.js
@@ -4,11 +4,15 @@ const inputTanggal = document.getElementById("tanggal");
 const daftarTugas = document.getElementById("listTugas");
 
 btnTambahTodo.addEventListener("click",function(){
-    
+
     if(inputValue.value === ""){
         alert("input tidak boleh kosong!");
         return;
     }
+
+    const listBaru = document.createElement("li");
+    const span = document.createElement("span");
+    span.innerHTML = inputValue.value;
 
 
 });
